37529d7f80a9fa6d09da78b7da672fe1c8f912ea
Leon <Leon.Engineer@hotmail.com>
2025-07-12 13:35:57 +0800

    consts

---
+++ uHand2.SDK/HandCommands.cs
@@ -0,0 +1,12 @@
+﻿namespace uHand2.SDK;
+
+public enum HandCommands : byte
+{
+    SingleServoMove = 2,
+    MultipleServoMove = 3,
+    ServoStop = 4,
+    FullActionRun = 6,
+    FullActionStop = 7,
+    FullActionErase = 8,
+    ActionDownload = 25,
+}
---
+++ uHand2.SDK/HandContracts.cs
@@ -0,0 +1,12 @@
+﻿namespace uHand2.SDK;
+
+public static class HandContracts
+{
+    public const int ServosCount = 6;
+    public const int AngleMin = 900;
+    public const int AngleDefault = 1500;
+    public const int AngleMax = 2000;
+    public const int ArgumentsMaxLength = 30;
+    public const int ActionItemsMaxCount = 255;
+    public const ushort DefaultBuffer16 = UInt16.MaxValue;
+}
