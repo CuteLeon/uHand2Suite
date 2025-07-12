cb51b7a681940906022e70c27a03a3fd54ccab33
Leon <Leon.Engineer@hotmail.com>
2025-07-12 22:51:12 +0800

    Server

--- uHand2.Server/Program.cs
+++ uHand2.Server/Program.cs
@@ -1,42 +1,64 @@
-using System.Text.Json.Serialization;
+using System.Net.WebSockets;
+using System.Text;
 
 namespace uHand2.Server;
 public class Program
 {
+    private static WebSocket? forwardWebSocket;
+
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateSlimBuilder(args);
-
-        builder.Services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-        });
-
         var app = builder.Build();
 
-        var sampleTodos = new Todo[] {
-            new(1, "Walk the dog"),
-            new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-            new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-            new(4, "Clean the bathroom"),
-            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-        };
+        app.Map("/ws", async context =>
+        {
+            Console.WriteLine($"/ws: Request: {context.Connection.Id}");
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                Program.forwardWebSocket = webSocket;
+                Console.WriteLine($"/ws: Connected.");
 
-        var todosApi = app.MapGroup("/todos");
-        todosApi.MapGet("/", () => sampleTodos);
-        todosApi.MapGet("/{id}", (int id) =>
-            sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-                ? Results.Ok(todo)
-                : Results.NotFound());
+                var buffer = new byte[1024 * 4];
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by server", CancellationToken.None);
+                        Program.forwardWebSocket = null;
+                        Console.WriteLine($"/ws: Disconnected.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"/ws: ReceiveData: [{result.MessageType}] [{result.Count}] {string.Join(",", buffer.Take(result.Count))}");
+                    }
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        });
 
+        app.MapPost("/forward", async (HttpContext context) =>
+        {
+            Console.WriteLine($"/forward: Request: {context.Connection.Id}");
+            using var reader = new StreamReader(context.Request.Body, Encoding.UTF8);
+            var body = await reader.ReadToEndAsync();
+            var bytes = Encoding.UTF8.GetBytes(body);
+
+            if (forwardWebSocket is not null)
+            {
+                Console.WriteLine($"/forward: Forward data to WebSocket...");
+                await forwardWebSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+
+            return Results.Ok();
+        });
+
+        app.UseWebSockets();
         app.Run();
     }
-}
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
 }
