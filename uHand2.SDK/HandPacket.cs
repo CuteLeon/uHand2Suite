1a124cf38acc62b12265384e961bdad0c34fb503
Leon <Leon.Engineer@hotmail.com>
2025-07-12 13:54:47 +0800

    Hand Packet Servo

---
+++ uHand2.SDK/HandPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace uHand2.SDK;
+
+public record class HandPacket
+{
+    // BinaryPrimitives.WriteInt32BigEndian MemoryStream BinaryWriter stackalloc  Span<>
+    public HandCommands Commands { get; set; }
+    public byte ServoCount { get; set; }
+    public ushort Time { get; set; }
+    public List<Servo>? Servos { get; set; }
+    public Servo? Servo { get; set; }
+}
---
+++ uHand2.SDK/Servo.cs
@@ -0,0 +1,8 @@
+﻿namespace uHand2.SDK;
+
+public record struct Servo
+{
+    public byte ServoId;
+
+    public ushort Angle;
+}
