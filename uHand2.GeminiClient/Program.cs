b70438e1493a2465589c177255e089145d949ed0
Leon <Leon.Engineer@hotmail.com>
2025-07-17 18:00:53 +0800

    GeminiClient

--- uHand2.GeminiClient/Program.cs
+++ uHand2.GeminiClient/Program.cs
@@ -6,63 +6,70 @@ using Microsoft.Extensions.AI;
 using uHand2.Contract;
 using uHand2.SDK;
 
-Console.WriteLine("Hello, World!");
-const string APIKey = "";
-
-using var communicator = new SerialPortCommunicator();
-Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Detecting valid Serial Port...");
-while (!communicator.DetectCommunicatePort())
+internal class Program
 {
-    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Didn't find valid Serial Port, retry ...");
-    await Task.Delay(1000);
-}
-Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Serial Port Connected.");
+    static SerialPortCommunicator communicator = default!;
 
-communicator.SendHandPacket(HandPacket.ResetPacket);
-
-var geminiClientOptions = new GeminiClientOptions
-{
-    ApiKey = APIKey,
-    ModelId = GeminiModels.Gemini2Flash,
-    ApiVersion = GeminiApiVersions.V1Beta,
-};
-var chatOptions = new ChatOptions()
-{
-    Tools = [AIFunctionFactory.Create(ControlHand, nameof(ControlHand))]
-};
-var geminiClient = new GeminiChatClient(geminiClientOptions);
-var client = new ChatClientBuilder(geminiClient).UseFunctionInvocation().Build();
-
-while (true)
-{
-    Console.WriteLine("===================================================");
-    (int cursorLeft, int cursorTop) = Console.GetCursorPosition();
-    Console.Write("Leon: ");
-    var userInput = Console.ReadLine();
-    if (string.IsNullOrEmpty(userInput)) break;
-
-    Console.SetCursorPosition(cursorLeft, cursorTop);
-    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Leon: {userInput}");
-    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Gemini: ");
-    try
+    static async Task Main(string[] args)
     {
-        await foreach (var update in client.GetStreamingResponseAsync(userInput, chatOptions))
+        Console.WriteLine("Hello, World!");
+        const string APIKey = "";
+
+        communicator = new SerialPortCommunicator();
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Detecting valid Serial Port...");
+        while (!communicator.DetectCommunicatePort())
         {
-            Console.Write(update);
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Didn't find valid Serial Port, retry ...");
+            await Task.Delay(1000);
         }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"[Exception] {ex.Message}");
-    }
-    Console.WriteLine();
-}
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Serial Port Connected.");
 
-Console.ReadLine();
-communicator.Dispose();
+        communicator.SendHandPacket(HandPacket.ResetPacket);
 
-[Description(
-    """
+        var geminiClientOptions = new GeminiClientOptions
+        {
+            ApiKey = APIKey,
+            ModelId = GeminiModels.Gemini2Flash,
+            ApiVersion = GeminiApiVersions.V1Beta,
+        };
+        var chatOptions = new ChatOptions()
+        {
+            Tools = [AIFunctionFactory.Create(ControlHand, nameof(ControlHand))]
+        };
+        var geminiClient = new GeminiChatClient(geminiClientOptions);
+        var client = new ChatClientBuilder(geminiClient).UseFunctionInvocation().Build();
+
+        while (true)
+        {
+            Console.WriteLine("===================================================");
+            (int cursorLeft, int cursorTop) = Console.GetCursorPosition();
+            Console.Write("Leon: ");
+            var userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput)) break;
+
+            Console.SetCursorPosition(cursorLeft, cursorTop);
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Leon: {userInput}");
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Gemini: ");
+            try
+            {
+                await foreach (var update in client.GetStreamingResponseAsync(userInput, chatOptions))
+                {
+                    Console.Write(update);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Exception] {ex.Message}");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+        communicator.Dispose();
+    }
+
+    [Description(
+        """
         此工具用于控制一个外部的右手机械手硬件。该机械手包含多个伺服传动装置，能够分别控制五根手指的伸直与弯曲，以及手腕的左右旋转。
         通过传入不同的角度参数，可以使机械手摆出各种手势，同时还能通过时间参数控制动作的快慢。
         返回参数为Boolean类型，true表示姿势执行成功。
@@ -133,25 +140,26 @@ communicator.Dispose();
             用户未明确说明时，可以静默使用null作为此参数的默认值传入此方法中。
             示例值: 1500 (表示中间位置)
         """)]
-static bool ControlHand(ushort durationMs = 1000, ushort? thumbAngle = null, ushort? indexFingerAngle = null, ushort? middleFingerAngle = null, ushort? ringFingerAngle = null, ushort? pinkyFingerAngle = null, ushort? wristRotationAngle = null)
-{
-    Debug.Print($"{DateTime.Now:HH:mm:ss.fff} [AIFunctionCalling] [ControlHand]: {durationMs:N0}ms, Thumb:{thumbAngle,-4:N0}, Index:{indexFingerAngle,-4:N0}, Middle:{middleFingerAngle,-4:N0}, Ring:{ringFingerAngle,-4:N0}, Pinky:{pinkyFingerAngle,-4:N0}, Wrist:{wristRotationAngle,-4:N0}");
-    var handPacket = new HandPacket() { Command = HandCommands.MultipleServoMove, Time = durationMs, Servos = new List<Servo>(HandContracts.ServosTotal) };
-    if (thumbAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.Thumb, thumbAngle.Value));
-    if (indexFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.IndexFinger, indexFingerAngle.Value));
-    if (middleFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.MiddleFinger, middleFingerAngle.Value));
-    if (ringFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.RingFinger, ringFingerAngle.Value));
-    if (pinkyFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.PinkyFinger, pinkyFingerAngle.Value));
-    if (wristRotationAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.Wrist, wristRotationAngle.Value));
-    try
+    static bool ControlHand(ushort durationMs = 1000, ushort? thumbAngle = null, ushort? indexFingerAngle = null, ushort? middleFingerAngle = null, ushort? ringFingerAngle = null, ushort? pinkyFingerAngle = null, ushort? wristRotationAngle = null)
     {
-        Debug.Print($"{DateTime.Now:HH:mm:ss.fff} [AIFunctionCalling] [ControlHand]: Sending packet on communicator: {handPacket}");
-        communicator.SendHandPacket(handPacket);
-        return true;
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"[AIFunctionCalling] [ControlHand]: Failed to process command: {ex.Message}");
-        return false;
+        Debug.Print($"{DateTime.Now:HH:mm:ss.fff} [AIFunctionCalling] [ControlHand]: {durationMs:N0}ms, Thumb:{thumbAngle,-4:N0}, Index:{indexFingerAngle,-4:N0}, Middle:{middleFingerAngle,-4:N0}, Ring:{ringFingerAngle,-4:N0}, Pinky:{pinkyFingerAngle,-4:N0}, Wrist:{wristRotationAngle,-4:N0}");
+        var handPacket = new HandPacket() { Command = HandCommands.MultipleServoMove, Time = durationMs, Servos = new List<Servo>(HandContracts.ServosTotal) };
+        if (thumbAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.Thumb, thumbAngle.Value));
+        if (indexFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.IndexFinger, indexFingerAngle.Value));
+        if (middleFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.MiddleFinger, middleFingerAngle.Value));
+        if (ringFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.RingFinger, ringFingerAngle.Value));
+        if (pinkyFingerAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.PinkyFinger, pinkyFingerAngle.Value));
+        if (wristRotationAngle.HasValue) handPacket.Servos.Add(new Servo(HandServos.Wrist, wristRotationAngle.Value));
+        try
+        {
+            Debug.Print($"{DateTime.Now:HH:mm:ss.fff} [AIFunctionCalling] [ControlHand]: Sending packet on communicator: {handPacket}");
+            communicator.SendHandPacket(handPacket);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[AIFunctionCalling] [ControlHand]: Failed to process command: {ex.Message}");
+            return false;
+        }
     }
 }
